d0e14b58ce9a8f1714ba35ecb1b929c618c77997
Andy Kluger <AndyKluger@gmail.com>
2023-11-02 16:25:44 -0400

    ripgrep aliases: use --hidden, and long options only

--- grep.zsh
+++ grep.zsh
@@ -1,8 +1,8 @@
 # -- ripgrep sugar --
-alias rg="=rg -S"
-alias rg1="=rg -S --maxdepth 1"
-alias rg2="=rg -S --maxdepth 2"
-alias rgm="=rg -S -U --multiline-dotall"
+alias rg="=rg --smart-case --hidden"
+alias rg1="=rg --smart-case --hidden --maxdepth 1"
+alias rg2="=rg --smart-case --hidden --maxdepth 2"
+alias rgm="=rg --smart-case --hidden --multiline --multiline-dotall"
 
 # -- grep --
 g () {  # <grep-arg>...

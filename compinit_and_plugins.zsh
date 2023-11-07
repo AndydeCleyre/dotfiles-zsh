2f8e33b22de8b86d1b90e990578640b0d2475a3d
Andy Kluger <AndyKluger@gmail.com>
2023-11-07 01:18:59 -0500

    Remove some escaped line breaks

--- compinit_and_plugins.zsh
+++ compinit_and_plugins.zsh
@@ -5,9 +5,6 @@
 # Configure plugins
 # -----------------
 
-# -- agkozak-zsh-prompt --
-# see prompt.zsh
-
 # -- zpy --
 # zstyle ':zpy:*' exposed-funcs zpy pipz a8 envin envout pipup vrun
 
@@ -20,15 +17,11 @@ ZSHZ_UNCOMMON=1
 # Load some plugins before compinit
 # ---------------------------------
 
-# -- Single Location Sources --
-.zshrc_load-plugin \
-  zsh-completions \
-  zsh-z
-
 # -- Generated Sources --
-.zshrc_fortnightly rtx \
-  ${XDG_DATA_HOME:-~/.local/share}/zsh/site-functions/_rtx \
-  rtx complete -s zsh
+.zshrc_fortnightly rtx ${XDG_DATA_HOME:-~/.local/share}/zsh/site-functions/_rtx rtx complete -s zsh || true
+
+# -- Load if found --
+.zshrc_load-plugin zsh-completions zsh-z
 
 # --------
 # compinit
@@ -42,25 +35,12 @@ compinit -i -d ${XDG_CACHE_HOME:-~/.cache}/zsh/zcompdump-$ZSH_VERSION
 # Load some plugins after compinit
 # --------------------------------
 
-# -- Alternate Location Sources --
-.zshrc_load-plugin --try \
-  /usr/share/doc/pkgfile/command-not-found.zsh \
-  /etc/zsh_command_not_found
-.zshrc_load-plugin --try \
-  ~/Code/zpy \
-  ${ZSH_PLUGINS_DIR}/zpy
-
-# -- Single Location Sources --
-.zshrc_load-plugin \
-  fast-syntax-highlighting \
-  agkozak-zsh-prompt \
-  zsh-autoenv
-
-if ! (( $+functions[agkozak-zsh-prompt_plugin_unload] )) {
-  .zshrc_load-plugin --try \
-    ${ZSH_PLUGINS_DIR}/powerlevel10k/powerlevel10k.zsh-theme
-}
-
 # -- Generated Sources --
 if { .zshrc_fortnightly pip ${ZSH_PLUGINS_DIR}/pip.zsh pip completion -z }  . ${ZSH_PLUGINS_DIR}/pip.zsh
 if { .zshrc_fortnightly rtx ${ZSH_PLUGINS_DIR}/rtx.zsh rtx activate zsh  }  . ${ZSH_PLUGINS_DIR}/rtx.zsh
+
+# -- Load if found --
+.zshrc_load-plugin fast-syntax-highlighting agkozak-zsh-prompt
+if ! (( $+functions[agkozak-zsh-prompt_plugin_unload] ))  .zshrc_load-plugin --try ${ZSH_PLUGINS_DIR}/powerlevel10k/powerlevel10k.zsh-theme
+.zshrc_load-plugin --try /usr/share/doc/pkgfile/command-not-found.zsh /etc/zsh_command_not_found
+.zshrc_load-plugin --try ~/Code/zpy ${ZSH_PLUGINS_DIR}/zpy

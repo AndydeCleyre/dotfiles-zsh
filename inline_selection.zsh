1c46b322b02efd2bb82100509f98d22592199c1c
Andy Kluger <AndyKluger@gmail.com>
2023-12-21 00:11:50 -0500

    Add surround helpers for inline selections

--- inline_selection.zsh
+++ inline_selection.zsh
@@ -1,5 +1,3 @@
-# Credit: https://stackoverflow.com/a/30899296
-
 # -- Select all --
 # Key: ctrl+a
 .zle_select-all () {
@@ -8,79 +6,61 @@
   (( REGION_ACTIVE=1 ))
   if (( $+functions[_zsh_highlight] ))  _zsh_highlight
 }
-zle -N .zle_select-all
+zle -N       .zle_select-all
 bindkey '^A' .zle_select-all  # ctrl+a
 
-.zshrc_def-select-and () {  # <action> <wrapped-zle> <keyseq>...
-
-  eval "
-    .zle_select-and-$1 () {
-      if ! (( REGION_ACTIVE ))  zle .set-mark-command
-      zle $2
-      if (( \$+functions[_zsh_highlight] ))  _zsh_highlight
-    }
-    zle -N .zle_select-and-$1
-    for keyseq ( ${(q)@[3,-1]} )  bindkey \$keyseq .zle_select-and-$1
-  "
-
+# -- Surround selection with single quote, or type single quote --
+# Key: '
+.zle_squote-selection-or-squote () {
+  if (( REGION_ACTIVE )) {
+    zle quote-region
+  } else {
+    LBUFFER+="'"
+  }
 }
+zle -N      .zle_squote-selection-or-squote
+bindkey "'" .zle_squote-selection-or-squote  # '
 
-.zshrc_def-deselect-and () {  # <action> <wrapped-zle> <keyseq>...
-
+# -- Surround selection with an open-close pair ({}, (), etc.), or type the opener --
+# Keys:
+#   - {
+#   - [
+#   - (
+#   - "
+.zshrc_def-surround-selection-or-type-opener () {  # <opener> <closer> <name>
   eval "
-    .zle_deselect-and-$1 () {
-      (( REGION_ACTIVE=0 ))
-      zle $2
-      if (( \$+functions[_zsh_highlight] ))  _zsh_highlight
-    }
-    zle -N .zle_deselect-and-$1
-    for keyseq ( ${(q)@[3,-1]} )  bindkey \$keyseq .zle_deselect-and-$1
-  "
-
-}
-
-.zshrc_def-del-selected-or () {  # <action> <wrapped-zle> <keyseq>...
-
-  eval "
-    .zle_del-selected-or-$1 () {
+    .zle_$3-selection-or-$3 () {
       if (( REGION_ACTIVE )) {
-        zle .kill-region
+        local start_end=(\$CURSOR \$MARK)
+        start_end=(\${(n)start_end})
+
+        local left=\${BUFFER[1,start_end[1]]}
+        local right=\${BUFFER[start_end[-1]+1,#BUFFER]}
+        local middle=${(q-)1}\${BUFFER[start_end[1]+1,start_end[-1]]}${(q-)2}
+
+        BUFFER=\${left}\${middle}\${right}
+
+        (( REGION_ACTIVE=0 ))
+        (( CURSOR+=1 ))
       } else {
-        zle $2
+        LBUFFER+=${(q-)1}
       }
       if (( \$+functions[_zsh_highlight] ))  _zsh_highlight
     }
-    zle -N .zle_del-selected-or-$1
-    for keyseq ( ${(q)@[3,-1]} )  bindkey \$keyseq .zle_del-selected-or-$1
+    zle -N           .zle_$3-selection-or-$3
+    bindkey ${(q-)1} .zle_$3-selection-or-$3
   "
-
 }
+.zshrc_def-surround-selection-or-type-opener '{' '}' brace
+.zshrc_def-surround-selection-or-type-opener '[' ']' bracket
+.zshrc_def-surround-selection-or-type-opener '(' ')' paren
+.zshrc_def-surround-selection-or-type-opener '"' '"' dquote
+unfunction .zshrc_def-surround-selection-or-type-opener
 
-() {
-  emulate -L zsh
-
-  local keyseq
-
-  .zshrc_def-deselect-and left       .backward-char '^[[D'                        # left
-  .zshrc_def-deselect-and right      .forward-char '^[[C'                         # right
-  .zshrc_def-deselect-and left-word  .backward-word '^[[1;5D' '^[[5~'             # {ctrl+left,pgup}
-  .zshrc_def-deselect-and right-word .forward-word '^[[1;5C' '^[[6~'              # {ctrl+right,pgdown}
-  .zshrc_def-deselect-and home       .beginning-of-line '^[[1;5A' '^[[H' '^[[1~'  # home
-  .zshrc_def-deselect-and end        .end-of-line '^[[1;5B' '^[[F' '^[[4~'        # end
-
-  .zshrc_def-select-and left       .backward-char '^[[1;2D'                # shift+left
-  .zshrc_def-select-and right      .forward-char '^[[1;2C'                 # shift+right
-  .zshrc_def-select-and left-word  .backward-word '^[[1;6D' '^[[5;2~'      # {ctrl+shift+left,shift+pgup}
-  .zshrc_def-select-and right-word .forward-word '^[[1;6C' '^[[6;2~'       # {ctrl+shift+right,shift+pgdown}
-  .zshrc_def-select-and home       .beginning-of-line '^[[1;2H' '^[[1;6A'  # {shift+home,ctrl+shift+up}
-  .zshrc_def-select-and end        .end-of-line '^[[1;2F' '^[[1;6B'        # {shift+end,ctrl+shift+down}
-
-  .zshrc_def-del-selected-or bksp .backward-delete-char '^?'             # backspace
-  .zshrc_def-del-selected-or del  .delete-char '^[[3~' "^[3'5~" '\e[3~'  # delete
-
-}
-
-unfunction .zshrc_def-select-and .zshrc_def-deselect-and .zshrc_def-del-selected-or
+# --------------------------------
+# -- Move cursor and (de)select --
+# --------------------------------
+# Credit: https://stackoverflow.com/a/30899296
 
 # -- Cursor left and deselect --
 # Key: left
@@ -135,3 +115,66 @@ unfunction .zshrc_def-select-and .zshrc_def-deselect-and .zshrc_def-del-selected
 
 # -- Delete or delete selection --
 # Key: del
+
+.zshrc_def-select-and () {  # <action> <wrapped-zle> <keyseq>...
+  eval "
+    .zle_select-and-$1 () {
+      if ! (( REGION_ACTIVE ))  zle .set-mark-command
+      zle $2
+      if (( \$+functions[_zsh_highlight] ))  _zsh_highlight
+    }
+    zle -N                                         .zle_select-and-$1
+    for keyseq ( ${(q)@[3,-1]} )  bindkey \$keyseq .zle_select-and-$1
+  "
+}
+
+.zshrc_def-deselect-and () {  # <action> <wrapped-zle> <keyseq>...
+  eval "
+    .zle_deselect-and-$1 () {
+      (( REGION_ACTIVE=0 ))
+      zle $2
+      if (( \$+functions[_zsh_highlight] ))  _zsh_highlight
+    }
+    zle -N                                         .zle_deselect-and-$1
+    for keyseq ( ${(q)@[3,-1]} )  bindkey \$keyseq .zle_deselect-and-$1
+  "
+}
+
+.zshrc_def-del-selected-or () {  # <action> <wrapped-zle> <keyseq>...
+  eval "
+    .zle_del-selected-or-$1 () {
+      if (( REGION_ACTIVE )) {
+        zle .kill-region
+      } else {
+        zle $2
+      }
+      if (( \$+functions[_zsh_highlight] ))  _zsh_highlight
+    }
+    zle -N                                         .zle_del-selected-or-$1
+    for keyseq ( ${(q)@[3,-1]} )  bindkey \$keyseq .zle_del-selected-or-$1
+  "
+}
+
+() {
+  emulate -L zsh
+
+  local keyseq
+
+  .zshrc_def-deselect-and left       .backward-char '^[[D'                        # left
+  .zshrc_def-deselect-and right      .forward-char '^[[C'                         # right
+  .zshrc_def-deselect-and left-word  .backward-word '^[[1;5D' '^[[5~'             # {ctrl+left,pgup}
+  .zshrc_def-deselect-and right-word .forward-word '^[[1;5C' '^[[6~'              # {ctrl+right,pgdown}
+  .zshrc_def-deselect-and home       .beginning-of-line '^[[1;5A' '^[[H' '^[[1~'  # home
+  .zshrc_def-deselect-and end        .end-of-line '^[[1;5B' '^[[F' '^[[4~'        # end
+
+  .zshrc_def-select-and left       .backward-char '^[[1;2D'                # shift+left
+  .zshrc_def-select-and right      .forward-char '^[[1;2C'                 # shift+right
+  .zshrc_def-select-and left-word  .backward-word '^[[1;6D' '^[[5;2~'      # {ctrl+shift+left,shift+pgup}
+  .zshrc_def-select-and right-word .forward-word '^[[1;6C' '^[[6;2~'       # {ctrl+shift+right,shift+pgdown}
+  .zshrc_def-select-and home       .beginning-of-line '^[[1;2H' '^[[1;6A'  # {shift+home,ctrl+shift+up}
+  .zshrc_def-select-and end        .end-of-line '^[[1;2F' '^[[1;6B'        # {shift+end,ctrl+shift+down}
+
+  .zshrc_def-del-selected-or bksp .backward-delete-char '^?'             # backspace
+  .zshrc_def-del-selected-or del  .delete-char '^[[3~' "^[3'5~" '\e[3~'  # delete
+}
+unfunction .zshrc_def-select-and .zshrc_def-deselect-and .zshrc_def-del-selected-or

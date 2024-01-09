9af5217c47c6ffb9adffb688b2b6022e3cffb2b7
Andy Kluger <AndyKluger@gmail.com>
2024-01-09 01:47:31 -0500

    rtx becomes mise

--- compinit_and_plugins.zsh
+++ compinit_and_plugins.zsh
@@ -18,7 +18,7 @@ ZSHZ_UNCOMMON=1
 # ---------------------------------
 
 # -- Generated Sources --
-.zshrc_fortnightly rtx   ${XDG_DATA_HOME:-~/.local/share}/zsh/site-functions/_rtx   rtx complete -s zsh        || true
+.zshrc_fortnightly mise  ${XDG_DATA_HOME:-~/.local/share}/zsh/site-functions/_mise  mise complete -s zsh       || true
 .zshrc_fortnightly prqlc ${XDG_DATA_HOME:-~/.local/share}/zsh/site-functions/_prqlc prqlc shell-completion zsh || true
 
 # -- Load if found --
@@ -37,8 +37,8 @@ compinit -i -d ${XDG_CACHE_HOME:-~/.cache}/zsh/zcompdump-$ZSH_VERSION
 # --------------------------------
 
 # -- Generated Sources --
-if { .zshrc_fortnightly pip ${ZSH_PLUGINS_DIR}/pip.zsh pip completion -z }  . ${ZSH_PLUGINS_DIR}/pip.zsh
-if { .zshrc_fortnightly rtx ${ZSH_PLUGINS_DIR}/rtx.zsh rtx activate zsh  }  . ${ZSH_PLUGINS_DIR}/rtx.zsh
+if { .zshrc_fortnightly pip  ${ZSH_PLUGINS_DIR}/pip.zsh pip completion -z  }  . ${ZSH_PLUGINS_DIR}/pip.zsh
+if { .zshrc_fortnightly mise ${ZSH_PLUGINS_DIR}/mise.zsh mise activate zsh }  . ${ZSH_PLUGINS_DIR}/mise.zsh
 
 # -- Load if found --
 .zshrc_load-plugin fast-syntax-highlighting agkozak-zsh-prompt

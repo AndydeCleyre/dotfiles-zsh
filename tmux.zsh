c3dd0fae9d4042cf19af76267acd4aaab48e6896
Andy Kluger <AndyKluger@gmail.com>
2024-04-12 14:15:57 -0400

    Make pgup find prompt lines immediately

--- tmux.zsh
+++ tmux.zsh
@@ -4,7 +4,11 @@ if [[ $TMUX ]] {
 
   # -- Activate copy mode and scroll up a page --
   # Key: pgup
-  .zle_tmux-copy-mode-pgup () { tmux copy-mode -u }
+  .zle_tmux-copy-mode-pgup () {
+    tmux \
+      copy-mode -u \; \
+      send -X search-backward '^-- '  # jump to prompt prefix
+  }
   zle -N          .zle_tmux-copy-mode-pgup
   bindkey '^[[5~' .zle_tmux-copy-mode-pgup  # pgup
 

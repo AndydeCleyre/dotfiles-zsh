911699fa16593edffae0aa0ced1ef3c9b003770f
Andy Kluger <AndyKluger@gmail.com>
2024-06-14 10:57:22 -0400

    leaner tmux conf

--- tmux.zsh
+++ tmux.zsh
@@ -1,25 +1,17 @@
 if (( $+commands[tmux] )) {  # Without tmux, skip this file
 
-if [[ $TMUX ]] {
+if ! [[ $TMUX ]] { tmux new -A } else {  # Join or create a session in not in one already
 
-  # -- Activate copy mode and scroll up a page --
-  # Key: pgup
-  .zle_tmux-copy-mode-pgup () {
-    tmux \
-      copy-mode -u \; \
-      send -X search-backward '^-- '  # jump to prompt prefix
-  }
-  zle -N          .zle_tmux-copy-mode-pgup
-  bindkey '^[[5~' .zle_tmux-copy-mode-pgup  # pgup
-
-} else {
-
-  # -- Join or create a session --
-  tmux new -A
+# -- Activate copy mode and scroll up a page --
+# Key: pgup
+.zle_tmux-copy-mode-pgup () {
+  tmux \
+    copy-mode -u \; \
+    send -X search-backward '^-- '  # jump to prompt prefix
+}
+zle -N          .zle_tmux-copy-mode-pgup
+bindkey '^[[5~' .zle_tmux-copy-mode-pgup  # pgup
 
 }
-
-# TODO: delete this, probably:
-alias t="tmux new -A"
 
 }

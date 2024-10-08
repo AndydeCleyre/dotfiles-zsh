55a59a44fac85e9fc530d80ed5aee570a1d93b35
Andy Kluger <AndyKluger@gmail.com>
2024-10-08 12:21:47 -0400

    tmux-select last output block

--- tmux.zsh
+++ tmux.zsh
@@ -12,6 +12,20 @@ if ! [[ $TMUX ]] { tmux new -A } else {  # Join or create a session if not in on
 zle -N          .zle_tmux-copy-mode-pgup
 bindkey '^[[5~' .zle_tmux-copy-mode-pgup  # pgup
 
+# -- Activate copy mode and select the last output block --
+# Key: shift+pgup
+.zle_tmux-copy-mode-last-output () {
+  tmux \
+    copy-mode \; \
+    send -X previous-paragraph \; \
+    send -X begin-selection \; \
+    send -X search-backward '^-- ' \; \
+    send -X cursor-down \; \
+    send -X stop-selection
+}
+zle -N            .zle_tmux-copy-mode-last-output
+bindkey '^[[5;2~' .zle_tmux-copy-mode-last-output  # shift+pgup
+
 }
 
 }

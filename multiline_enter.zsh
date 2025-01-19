8d3ce1291abf36b35e5e7d24c36c7e0702a6821c
Andy Kluger <AndyKluger@gmail.com>
2025-01-18 23:31:41 -0500

    simplify multiline enter with zle message

--- multiline_enter.zsh
+++ multiline_enter.zsh
@@ -1,6 +1,5 @@
 # -- Run input if single line, otherwise insert newline --
 # Key: enter
-# Assumes: setopt interactivecomments
 # Credit: https://programming.dev/comment/2479198
 .zle_accept-except-multiline () {
   if [[ $BUFFER != *$'\n'* ]] {
@@ -8,14 +7,7 @@
     return
   } else {
     zle .self-insert-unmeta
-    if [[ $BUFFER == *$'\n'*$'\n'* ]] {
-      local hint="# Use alt+enter to submit this multiline input"
-      if [[ $BUFFER != *${hint}* ]] {
-        LBUFFER+=$hint
-        zle .self-insert-unmeta
-        if (( $+functions[_zsh_highlight] ))  _zsh_highlight
-      }
-    }
+    zle -M 'Use alt+enter to submit this multiline input'
   }
 }
 zle -N       .zle_accept-except-multiline
